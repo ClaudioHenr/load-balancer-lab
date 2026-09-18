9de5c1067255cb6c35fd978a97a76c8cb5c35b3d
stromae <claudio.oliveira10.0p@gmail.com>
2026-09-17 23:36:44 -0300

    feat: implemented weigthed round robin algoritm

--- 02-weighted-round-robin/load-balancer/src/algorithms/roundRobin.ts
+++ 02-weighted-round-robin/load-balancer/src/algorithms/roundRobin.ts
@@ -1,10 +1,33 @@
-export class RoundRobin {
+export class WeightedRoundRobin {
   private index = 0;
+  private servers: { url: string; weight: number }[] = [];
+  private lengthServers = 0;
 
-  select<T>(items: T[]): T | undefined {
-    if (!items.length) return undefined;
-    const item = items[this.index % items.length];
-    this.index += 1;
-    return item;
+  constructor(servers: { url: string; weight: number }[]) {
+    this.servers = servers;
+    this.lengthServers = servers.length;
+  }
+
+  choiceServerToHandleRequest(): { url: string; weight: number } | string {
+    let server = "";
+    if (this.index < this.servers[0].weight) {
+      server = this.servers[0].url;
+      this.index = this.index + 1;
+      return server
+    }
+    if (this.index < this.servers[1].weight) {
+      server = this.servers[1].url;
+      this.index = this.index + 1;
+      return server
+    }
+    if (this.index < this.servers[2].weight) {
+      server = this.servers[2].url;
+      this.index = this.index + 1;
+      return server
+    }
+    this.index = 0;
+    server = this.servers[0].url;
+    this.index = this.index + 1;
+    return server
   }
 }
--- 02-weighted-round-robin/load-balancer/src/server.ts
+++ 02-weighted-round-robin/load-balancer/src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import { WeightedRoundRobin } from './algorithms/roundRobin';
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
+const servers = [
+  { url: 'http://localhost:3001', weight: 1 },
+  { url: 'http://localhost:3002', weight: 4 },
+  { url: 'http://localhost:3003', weight: 6 },
+];
+const weightedRoundRobin = new WeightedRoundRobin(servers);
 
 app.get('/health', (_, res) => {
   res.json({ status: 'ok', module: '02-weighted-round-robin' });
 });
+
+app.get('/api', async (req, res) => {
+  let server = weightedRoundRobin.choiceServerToHandleRequest();
+  res.json({ server });
+})
 
 app.listen(port, () => {
   console.log(`Load balancer running on port ${port}`);
